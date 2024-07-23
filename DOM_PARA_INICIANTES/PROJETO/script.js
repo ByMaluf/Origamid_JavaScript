b8b419c3aee9dd386a3fde180de8e0aee2deabe6
Brenno Ysrael M. L. Correia <brenno.maluf@gmail.com>
2024-07-23 12:52:20 -0400

    Script + HTML animais-lista

--- DOM_PARA_INICIANTES/PROJETO/script.js
+++ DOM_PARA_INICIANTES/PROJETO/script.js
@@ -1,17 +1,21 @@
 const tabMenu = document.querySelectorAll('.js-tabmenu li');
 const tabContent = document.querySelectorAll('.js-tabcontent section');
 
-function activeTab(index) {
-  tabContent.forEach((section) => section.classList.remove('ativo'));
-  tabContent[index].classList.add('ativo')
-}
+if (tabContent.length && tabMenu.length) {
+  tabContent[0].classList.add('ativo');
 
-function activeTabMenu() {
-  const index = Array.from(tabMenu).indexOf(this);
-  activeTab(index);
-}
+  function activeTab(index) {
+    tabContent.forEach((section) => section.classList.remove('ativo'));
+    tabContent[index].classList.add('ativo')
+  }
 
-tabMenu.forEach((li) => {
-  li.addEventListener('click', activeTabMenu);
-})
+  function activeTabMenu() {
+    const index = Array.from(tabMenu).indexOf(this);
+    activeTab(index);
+  }
+
+  tabMenu.forEach((li) => {
+    li.addEventListener('click', activeTabMenu);
+  })
+}
 
