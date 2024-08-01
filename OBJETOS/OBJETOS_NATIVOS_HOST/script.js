f5517aade841151da037bde92a1e9acf9c5cf37c
Brenno Maluf <brenno.maluf@gmail.com>
2024-08-01 16:34:48 -0400

    Objetos Nativos e Host

---
+++ OBJETOS/OBJETOS_NATIVOS_HOST/script.js
@@ -0,0 +1,24 @@
+// Exercício
+
+// Liste 5 objetos nativos
+
+String
+Object
+Array 
+Function
+Boolean
+
+// Liste 5 objetos do browser
+Window
+Document
+NodeList
+HTMLCollection
+Location
+
+// Liste 2 Métodos, Propriedades ou Objetos
+// presentes no Chrome que não existem no Firefox
+
+webkitHidden
+webkitVisibilityState
+
+
