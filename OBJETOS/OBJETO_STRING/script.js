24df704e6aa3a84986765b742db722ab012f3cf5
Brenno Maluf <brenno.maluf@gmail.com>
2024-08-01 17:15:52 -0400

    Objeto String

---
+++ OBJETOS/OBJETO_STRING/script.js
@@ -0,0 +1,10 @@
+/*
+String
+
+String
+É a construtora de strings, toda string possui as propriedades e métodos do prototype de String.
+*/
+
+const comida = 'Pizza'; // Retorna um String
+const liquido = new String('Água'); // Retorna um objeto do tipo String
+const ano = new String(2018); //Erro
