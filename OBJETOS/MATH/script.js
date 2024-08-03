8cf92797dd6bc9fe2ae3c48b858f9b0db9c59756
Brenno Ysrael M. L. Correia <brenno.maluf@gmail.com>
2024-08-03 07:49:06 -0400

    Atualização Objeto Math

--- OBJETOS/MATH/script.js
+++ OBJETOS/MATH/script.js
@@ -1,11 +1,10 @@
-"use strict";
 // Retorne um número aleatório
 // entre 1050 e 2000
 const numeroAletorio = Math.floor(Math.random() * (2000 - 1050 + 1)) + 1050;
 console.log(numeroAletorio)
 
 // Retorne o maior número da lista abaixo
-// const numeros = '4, 5, 20, 8, 9';
+const numeros = '4, 5, 20, 8, 9';
 console.log(Math.max(...numeros.split(',')));
 
 // Crie uma função para limpar os preços
@@ -13,7 +12,7 @@ console.log(Math.max(...numeros.split(',')));
 // depois retorne a soma total
 const listaPrecos = ['R$ 59,99', ' R$ 100,222',
   'R$ 230  ', 'r$  200'];
- 
+
 let total = 0;
 listaPrecos.forEach((preco) => {
   const minusculo = preco.toLocaleLowerCase();
@@ -22,3 +21,4 @@ listaPrecos.forEach((preco) => {
   total += numeroConvertido
 })
 console.log(total)
+
