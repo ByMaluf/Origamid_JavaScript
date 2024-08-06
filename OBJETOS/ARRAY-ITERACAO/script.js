c62cc2b8385cfbbe2f23ace635409b673f1fb28c
Brenno Ysrael M. L. Correia <brenno.maluf@gmail.com>
2024-08-05 22:08:57 -0400

    Exercício Array Iteração

--- OBJETOS/ARRAY-ITERACAO/script.js
+++ OBJETOS/ARRAY-ITERACAO/script.js
@@ -113,13 +113,87 @@ total1; // 65
 const total2 = aulas.reduce((acc, cur) => acc + cur, 100);
 total2; // 165
 
-
-
 const arrayDeNumeros = [100, 45, 3, 60];
 
 console.log(arrayDeNumeros.reduce((acumulador, valorAtual, index, array) => {
   return acumulador + valorAtual
 }, 0))
 
+//Exercícios
 
+// Selecione cada curso e retorne uma array
+// com objetos contendo o título, descricao,
+// aulas e horas de cada curso
 
+const cursos = document.querySelectorAll('.curso');
+const cursosArray = Array.from(cursos);
+const cursosDados = cursosArray.map((curso) => {
+  return {
+    titulo: curso.querySelector('h1').innerText,
+    descricao: curso.querySelector('p').innerText,
+    aulas: curso.querySelector('.aulas').innerText,
+    horas: curso.querySelector('.horas').innerText,
+  }
+})
+console.log(cursosDados)
+
+// Retorne uma lista com os
+// números maiores que 100
+const numeros = [3, 44, 333, 23, 122, 322, 33];
+
+console.log(numeros.reduce((acumulador, atual) => {
+  let maiorQueCem = atual > 100 ? acumulador.concat(atual) : acumulador
+  return maiorQueCem
+}, []))
+
+//Retorna um array com todos os valores truthy
+console.log(numeros.filter(function (numero) {
+  return numero > 100
+}))
+
+//Retornar o primeiro elemento maior que 100
+console.log(numeros.find((n) => {
+  return n > 100
+}))
+
+//Retorna o index do primeiro elemento truthy
+console.log(numeros.findIndex((n) => {
+  return n > 100
+}))
+
+// Verifique se Baixo faz parte
+// da lista de instrumentos e retorne true
+const instrumentos = ['Guitarra', 'Baixo', 'Bateria', 'Teclado', 'Baixo']
+console.log(instrumentos.some((palavra) => palavra === 'Baixo')); // retorna true para o primeiro lemento truthy;
+console.log(instrumentos.includes('Baixo')); // Verifica se existe algum 'Baixo' na array;
+
+// Retorne o valor total das compras
+const compras = [
+  {
+    item: 'Banana',
+    preco: 'R$ 4,99'
+  },
+  {
+    item: 'Ovo',
+    preco: 'R$ 2,99'
+  },
+  {
+    item: 'Carne',
+    preco: 'R$ 25,49'
+  },
+  {
+    item: 'Refrigerante',
+    preco: 'R$ 5,35'
+  },
+  {
+    item: 'Quejo',
+    preco: 'R$ 10,60'
+  }
+]
+
+const valorDasCompras = compras.reduce((valorTotal, valor, index, array) => {
+  let valores = +valor.preco.toUpperCase().replace('R$ ', '').replace(',', '.')
+  return valorTotal + valores;
+}, 0)
+
+console.log(valorDasCompras.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' }))
