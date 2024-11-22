9e4ff5d4468ef44da0edc25e461821d17407ebaf
Brenno Ysrael Maluf Luccas Correia <brenno.maluf@gmail.com>
2024-11-21 22:02:55 -0400

    Exercícios fetchs prontos.

--- JAVASCRIPT_ASSINCRONO/FETCH/script.js
+++ JAVASCRIPT_ASSINCRONO/FETCH/script.js
@@ -2,24 +2,54 @@
 // crie um formulário onde o usuário pode digitar o cep
 // e o endereço completo é retornado ao clicar em buscar
 
+const form = document.querySelector('form');
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const cepValue = form.cep.value;
+  viaCep(cepValue);
+})
+
+function viaCep(cep) {
+  const resultado = document.querySelector('.resultado');
+  const url = `https://viacep.com.br/ws/${cep}/json/`
+  fetch(url)
+    .then(data => data.text()).then(data => resultado.innerText = data);
+}
+
 // Utilizando a API https://blockchain.info/ticker
 // retorne no DOM o valor de compra da bitcoin and reais.
 // atualize este valor a cada 30s
+
+
+const btcDisplay = document.querySelector('.bitCoin');
+
+function fetchBtc() {
+  fetch('https://blockchain.info/ticker')
+    .then(r => r.json())
+    .then(body => btcDisplay.innerText = ('R$ ' + body.BRL.buy).replace('.', ','))
+}
+fetchBtc();
+
+// setInterval(fetchBtc, 4000);
 
 // Utilizando a API https://api.chucknorris.io/jokes/random
 // retorne uma piada randomica do chucknorris, toda vez que
 // clicar em próxima
 
+const next = document.querySelector('.next')
 
-const form = document.querySelector('form');
+next.addEventListener('click', fetchJoke);
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  const cep = form.cep.value;
-  viaCep(cep).then(cep => console.log(cep))
-})
-
-function viaCep(cep) {
-  const url = `https://viacep.com.br/ws/${cep}/json/`
-  return fetch(url).then(data => data.json());
+function fetchJoke() {
+  const joke = document.querySelector('.joke');
+  const imgJoke = document.querySelector('.imgJoke');
+  fetch('https://api.chucknorris.io/jokes/random')
+    .then(r => r.json())
+    .then(body => {
+      joke.innerText = body.value;
+      imgJoke.src = body.icon_url;
+    })
 }
+
+fetchJoke()
+
