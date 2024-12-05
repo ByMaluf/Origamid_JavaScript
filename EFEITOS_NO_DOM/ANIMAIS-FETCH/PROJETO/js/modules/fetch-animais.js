ecedfbf5e886817f5d02eafba72f518fbd74e0ec
Brenno Ysrael Maluf Luccas Correia <brenno.maluf@gmail.com>
2024-12-04 20:41:15 -0400

    Animais fetch finalizado

--- EFEITOS_NO_DOM/ANIMAIS-FETCH/PROJETO/js/modules/fetch-animais.js
+++ EFEITOS_NO_DOM/ANIMAIS-FETCH/PROJETO/js/modules/fetch-animais.js
@@ -1,3 +1,25 @@
-export default function initFetchAnimais(){
-  
+import initAnimaNumeros from "./anima-numeros.js";
+
+export default function initFetchAnimais() {
+  async function fetchData() {
+    const animaisResponse = await fetch('./animaisapi.json');
+    const animaisJSON = await animaisResponse.json();
+    animaisJSON.forEach((animal) => {
+      const container = document.querySelector('.numeros-grid');
+      container.appendChild(createAnimal(animal));
+    });
+    initAnimaNumeros();
+  }
+
+  function createAnimal(animal) {
+    const div = document.createElement('div');
+    div.classList.add('numero-animal');
+
+    div.innerHTML = `<h3>${animal.specie}</h3><span data-numero>${animal.total}</span>`
+    return div
+  }
+  fetchData()
 }
+
+
+
