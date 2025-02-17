5c07e63bb570a6f7fac3d676f798a7b32326688c
Brenno Ysrael Maluf Luccas Correia <brenno.maluf@gmail.com>
2025-02-17 12:58:45 -0400

    Refatoração bitcoin

--- BABEL/js/script.js
+++ BABEL/js/script.js
@@ -2,13 +2,13 @@ import ScrollSuave from './modules/scroll-suave.js';
 import Accordion from './modules/accordion.js';
 import TabNav from './modules/tabnav.js';
 import Modal from './modules/modal.js';
-import Tooltip from './modules/tooltip.js';
-import fetchAnimais from './modules/fetch-animais.js';
 import initDropdownMenu from './modules/dropdown-menu.js';
 import initMenuMobile from './modules/menu-mobile.js';
 import initFuncionamento from './modules/funcionamento.js';
-import initFetchBitcoin from './modules/fetch-bitcoin.js';
 import initAnimacaoScroll from './modules/scroll-animacao.js';
+import Tooltip from './modules/tooltip.js';
+import fetchAnimais from './modules/fetch-animais.js';
+import FetchBitcoin from './modules/fetch-bitcoin.js';
 
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
 scrollSuave.init();
@@ -25,9 +25,10 @@ modal.init();
 const tooltip = new Tooltip('[data-tooltip]');
 tooltip.init();
 
+
 initDropdownMenu();
 initMenuMobile();
 initFuncionamento();
-initFetchBitcoin();
 initAnimacaoScroll();
 fetchAnimais('./animaisapi.json', '.numeros-grid');
+FetchBitcoin('https://blockchain.info/ticker', '.btc-preco');
