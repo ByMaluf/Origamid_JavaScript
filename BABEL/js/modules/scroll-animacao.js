efb5f9f81e9e4716b72c8136c2a6e894df2f8b51
Brenno Ysrael Maluf Luccas Correia <brenno.maluf@gmail.com>
2025-02-17 17:27:27 -0400

    Debounce scroll-animações

--- BABEL/js/modules/scroll-animacao.js
+++ BABEL/js/modules/scroll-animacao.js
@@ -1,9 +1,10 @@
+import debounce from "./debounce";
 export default class ScrollAnima {
 
   constructor(section) {
     this.sections = document.querySelectorAll(section);
     this.windowMetade = window.innerHeight * 0.6;
-    this.checkDistance = this.checkDistance.bind(this);
+    this.checkDistance = debounce(this.checkDistance.bind(this), 50);
   }
 
   //Pega a distância de cada item em relação
