90b2b2d94e3e2a79554bae72b0b28e0bcfcc6205
Brenno Ysrael <brenno.maluf@gmail.com>
2025-02-22 17:37:22 -0400

    Final da navegação prev e nav

--- SLIDE/js/slide.js
+++ SLIDE/js/slide.js
@@ -11,6 +11,11 @@ export default class Slide {
     return this.dist.finalPosition - this.dist.movement;
   }
 
+  transition(active) {
+    this.slide.style.transition = active ? 'transform .3s' : '';
+
+  }
+
   moveSlide(distX) {
     this.dist.movePosition = distX;
     this.slide.style.transform = `translate3d(${distX}px, 0 ,0)`;
@@ -27,6 +32,7 @@ export default class Slide {
       moveType = 'touchmove';
     }
     this.wrapper.addEventListener(moveType, this.onMove)
+    this.transition(false);
   }
 
   onMove(event) {
@@ -39,6 +45,18 @@ export default class Slide {
     const moveType = (event.type === 'mouseup') ? 'mousemove' : 'touchmove';
     this.wrapper.removeEventListener(moveType, this.onMove);
     this.dist.finalPosition = this.dist.movePosition;
+    this.transition(true);
+    this.changeSlideOnEnd();
+  }
+
+  changeSlideOnEnd() {
+    if (this.dist.movement > 120 && this.index.next !== undefined) {
+      this.activeNextSlide();
+    } else if (this.dist.movement < -120 && this.index.prev !== undefined) {
+      this.activePrevSlide();
+    } else {
+      this.changeSlide(this.index.active);
+    }
   }
 
   addSlideEvents() {
@@ -55,7 +73,6 @@ export default class Slide {
   }
 
   //Slides config
-
   slidePosition(slide) {
     const margin = (this.wrapper.offsetWidth - slide.offsetWidth) / 2;
     return -(slide.offsetLeft - margin);
@@ -84,9 +101,20 @@ export default class Slide {
     this.dist.finalPosition = activeSlide.position
   }
 
+  activePrevSlide() {
+    if (this.index.prev !== undefined)
+      this.changeSlide(this.index.prev);
+  }
+
+  activeNextSlide() {
+    if (this.index.next !== undefined)
+      this.changeSlide(this.index.next);
+  }
+
 
   init() {
     this.bindEvents();
+    this.transition(true);
     this.addSlideEvents();
     this.slidesConfig();
     return this;
