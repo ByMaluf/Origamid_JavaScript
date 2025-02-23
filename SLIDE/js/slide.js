003dc967c4a94b805360ef2bddcd6146c586423d
Brenno Ysrael <brenno.maluf@gmail.com>
2025-02-23 10:57:39 -0400

    Ajuste das configurações ao resize e classe ativa adicionada.

--- SLIDE/js/slide.js
+++ SLIDE/js/slide.js
@@ -1,9 +1,11 @@
+import debounce from "./debounce.js";
 export default class Slide {
 
   constructor(slide, wrapper) {
     this.slide = document.querySelector(slide);
     this.wrapper = document.querySelector(wrapper);
     this.dist = { finalPosition: 0, startX: 0, movement: 0 }
+    this.activeClass = 'active'
   }
 
   updatePosition(clientX) {
@@ -66,12 +68,6 @@ export default class Slide {
     this.wrapper.addEventListener('touchend', this.onEnd);
   }
 
-  bindEvents() {
-    this.onStart = this.onStart.bind(this);
-    this.onEnd = this.onEnd.bind(this);
-    this.onMove = this.onMove.bind(this);
-  }
-
   //Slides config
   slidePosition(slide) {
     const margin = (this.wrapper.offsetWidth - slide.offsetWidth) / 2;
@@ -99,6 +95,12 @@ export default class Slide {
     this.moveSlide(activeSlide.position);
     this.slideIndexNav(index);
     this.dist.finalPosition = activeSlide.position
+    this.changeActiveClass();
+  }
+
+  changeActiveClass() {
+    this.slideArray.forEach(item => item.element.classList.remove(this.activeClass));
+    this.slideArray[this.index.active].element.classList.add(this.activeClass)
   }
 
   activePrevSlide() {
@@ -111,10 +113,29 @@ export default class Slide {
       this.changeSlide(this.index.next);
   }
 
+  onResize() {
+    console.log('teste');
+    setTimeout(() => {
+      this.slidesConfig();
+      this.changeSlide(this.index.active);
+    }, 1000);
+  }
+
+  addResizeEvent() {
+    window.addEventListener('resize', this.onResize);
+  }
+
+  bindEvents() {
+    this.onStart = this.onStart.bind(this);
+    this.onEnd = this.onEnd.bind(this);
+    this.onMove = this.onMove.bind(this);
+    this.onResize = debounce(this.onResize.bind(this), 50);
+  }
 
   init() {
     this.bindEvents();
     this.transition(true);
+    this.addResizeEvent();
     this.addSlideEvents();
     this.slidesConfig();
     return this;
